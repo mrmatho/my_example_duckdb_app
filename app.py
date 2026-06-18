aa9298918fc1bf46bf052216e895a28f9a7b569a
Geoff Matheson <geoff.matheson@education.vic.gov.au>
2026-06-18 13:11:01 +1000

    Initial Commit - setup dummy for validate_query and run_query

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import duckdb
+
+st.title("DuckDB SQL Query Workbench")
+
+def validate_query(sql_text) -> bool | str:
+    """
+    @param sql_text: The SQL query to validate.
+    @return: True if the query is valid
+    @throws: InvalidQueryError if the query is invalid.
+    """
+    return True
+
+def run_query(sql_text: str):
+    """
+    @param sql_text: The SQL query to run.
+    @return: The result of the query.
+    """
+    return None
+
